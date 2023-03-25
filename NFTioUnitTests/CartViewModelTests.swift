e2e728563d8d3f23b1525285689107f38522098f
darikutelov <dari@kutelov.com>
2023-03-25 17:47:47 +0200

    Unit testing - CartViewModel

--- NFTioUnitTests/CartViewModelTests.swift
+++ NFTioUnitTests/CartViewModelTests.swift
@@ -10,10 +10,13 @@ import XCTest
 @testable import NFTio
 
 final class CartViewModelTests: XCTestCase {
+    var viewModel: CartViewModel!
     var cartItem1: ShoppingCartItem!
     var cartItem2: ShoppingCartItem!
 
     override func setUpWithError() throws {
+        viewModel = CartViewModel()
+        viewModel.cartItems = []
         cartItem1 = ShoppingCartDataManager().shoppingCartItems[0]
         cartItem2 = ShoppingCartDataManager().shoppingCartItems[1]
     }
@@ -22,15 +25,49 @@ final class CartViewModelTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testAddToCartNewNFTItem() {
+    func testAddNewItemToCart() {
+        XCTAssertEqual(viewModel.cartItems.count, 0)
         
+        viewModel.addItemToCart(nft: cartItem1.nftItem)
+        XCTAssertEqual(viewModel.cartItems.count, 1)
+        XCTAssertEqual(viewModel.cartItems[0].quantity, 1)
+        
+        viewModel.addItemToCart(nft: cartItem2.nftItem)
+        XCTAssertEqual(viewModel.cartItems.count, 2)
+        XCTAssertEqual(viewModel.cartItems[1].quantity, 1)
     }
     
-    func testAddToCartExistingNFTItem() {
+    func testAddExistingItemInCart() {
+        XCTAssertEqual(viewModel.cartItems.count, 0)
         
+        viewModel.addItemToCart(nft: cartItem1.nftItem)
+        XCTAssertEqual(viewModel.cartItems.count, 1)
+        XCTAssertEqual(viewModel.cartItems[0].quantity, 1)
+        
+        viewModel.addItemToCart(nft: cartItem1.nftItem)
+        XCTAssertEqual(viewModel.cartItems.count, 1)
+        XCTAssertEqual(viewModel.cartItems[0].quantity, 2)
+    }
+    
+    func removeItemFromCart() {
+        viewModel.addItemToCart(nft: cartItem1.nftItem)
+        viewModel.addItemToCart(nft: cartItem2.nftItem)
+        XCTAssertEqual(viewModel.cartItems.count, 2)
+     
+        viewModel.removeItemFromCart(shoppingCardItem: cartItem1)
+        XCTAssertEqual(viewModel.cartItems.count, 2)
     }
     
     func testTotalAmount() {
+        XCTAssertEqual(viewModel.totalAmount, 0)
         
+        viewModel.addItemToCart(nft: cartItem1.nftItem)
+        var totalAmount = viewModel.totalAmount.rounded(2, .bankers)
+        XCTAssertEqual(totalAmount, 67.45)
+        
+        viewModel.addItemToCart(nft: cartItem2.nftItem)
+        totalAmount = viewModel.totalAmount.rounded(2, .bankers)
+        XCTAssertEqual(totalAmount, 134.85)
+    
     }
 }
