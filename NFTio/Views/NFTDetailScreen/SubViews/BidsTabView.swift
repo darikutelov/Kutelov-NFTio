75634b3c186bcfb3fdbd023d8b1f031eded31852
darikutelov <dari@kutelov.com>
2023-04-22 14:46:54 +0300

    Style bid price and crypto currency selection fields

--- NFTio/Views/NFTDetailScreen/SubViews/BidsTabView.swift
+++ NFTio/Views/NFTDetailScreen/SubViews/BidsTabView.swift
@@ -6,13 +6,15 @@
 //
 
 import SwiftUI
+import Combine
 
 struct BidsTabView: View {
     @EnvironmentObject var nftViewModel: NFTViewModel
     @EnvironmentObject var userViewModel: UserViewModel
-
-    @State var price: Decimal = 0
+    
+    @State var price: String = ""
     @State var bidCurrency: CryptoCurrency = CryptoCurrency.ethereum
+    @State var errorMessage = ""
     
     var body: some View {
         VStack {
@@ -24,10 +26,44 @@ struct BidsTabView: View {
                 }
             }
             
-
+            Section {
+                TextField("",
+                          text: $price,
+                          prompt: Text("Enter your bidding price")
+                    .foregroundColor(Color(Constants.Colors.charcoal).opacity(0.7)))
+                .keyboardType(.numbersAndPunctuation)
+                .onReceive(Just(price)) { newValue in
+                    let filtered = newValue.filter { "0123456789.".contains($0) }
+                    if filtered != newValue {
+                        self.price = filtered
+                    }
+                }
+                .modifier(StandardInputField(error: !errorMessage.isEmpty))
+                .padding(.bottom, Constants.Spacing.standard)
+                
+                Menu {
+                    Picker("Currency", selection: $bidCurrency) {
+                        ForEach(CryptoCurrency.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                } label: {
+                    HStack {
+                        Text("Select Crypto Currency")
+                            .foregroundColor(Color(Constants.Colors.charcoal).opacity(0.7))
+                            .padding()
+                        Spacer()
+                    }
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: Constants.Spacing.small)
+                        .stroke(Color(Constants.Colors.secondary), lineWidth: 1.0)
+                )
+                .frame(maxWidth: 400)
+                .padding(.bottom, Constants.Spacing.standard)
+            }
         }
     }
-        
 }
 
 struct BidsTabView_Previews: PreviewProvider {
